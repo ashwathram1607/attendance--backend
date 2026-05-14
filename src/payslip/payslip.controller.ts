5c9595bfa85352bf697806c7473f6b0e48e399cb
ashwathram1607 <ashwathram94@gmsil.com>
2026-05-14 19:05:52 +0530

    Updated payslip controller and API configuration

--- src/payslip/payslip.controller.ts
+++ src/payslip/payslip.controller.ts
@@ -1,14 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Delete,
-  Put,
-  ParseIntPipe,
-} from '@nestjs/common';
-
+import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
 import { PayslipService } from './payslip.service';
 import { CreatePayslipDto } from './dto/create-payslip.dto';
 
@@ -16,56 +6,28 @@ import { CreatePayslipDto } from './dto/create-payslip.dto';
 export class PayslipController {
   constructor(private readonly payslipService: PayslipService) {}
 
-  // =========================================================
-  // CREATE
-  // =========================================================
   @Post()
   create(@Body() createPayslipDto: CreatePayslipDto) {
     return this.payslipService.create(createPayslipDto);
   }
 
-  // =========================================================
-  // GET ALL PAYSLIPS
-  // =========================================================
   @Get()
   findAll() {
     return this.payslipService.findAll();
   }
 
-  // =========================================================
-  // STAFF ROUTE
-  // =========================================================
-  @Get('staff')
-  getStaff() {
-    return this.payslipService.findAll();
-  }
-
-  // =========================================================
-  // USERS ROUTE
-  // =========================================================
-  @Get('users')
-  getUsers() {
-    return this.payslipService.findAll();
-  }
-
-  // =========================================================
-  // GET SINGLE PAYSLIP
-  // IMPORTANT:
-  // Keep this route LAST
-  // =========================================================
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.payslipService.findOne(id);
+  findOne(@Param('id') id: string) {
+    return this.payslipService.findOne(+id);
   }
 
-  // =========================================================
-  // UPDATE
-  // =========================================================
   @Put(':id')
-  update(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() updatePayslipDto: CreatePayslipDto,
-  ) {
-    return this.payslipService.update(id, updatePayslipDto);
+  update(@Param('id') id: string, @Body() updatePayslipDto: CreatePayslipDto) {
+    return this.payslipService.update(+id, updatePayslipDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.payslipService.remove(+id);
   }
 }
