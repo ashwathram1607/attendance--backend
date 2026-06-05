eb218d45e430999cccc735cc46dbe10c2d33dcb3
ashwathram1607 <ashwathram94@gmsil.com>
2026-06-05 10:13:11 +0530

    fix balances route

--- src/leaves/leaves.controller.ts
+++ src/leaves/leaves.controller.ts
@@ -1,13 +1,4 @@
-import {
-  Controller,
-  Post,
-  Get,
-  Put,
-  Param,
-  Body,
-  ParseIntPipe,
-  Patch,
-} from '@nestjs/common';
+import { Controller, Post, Get, Put, Param, Body, ParseIntPipe } from '@nestjs/common';
 import { LeavesService } from './leaves.service';
 import { CreateLeaveDto } from './dto/create-leave.dto';
 import { UpdateLeaveDto } from './dto/update-leave.dto';
@@ -16,30 +7,36 @@ import { UpdateLeaveDto } from './dto/update-leave.dto';
 export class LeavesController {
   constructor(private readonly leavesService: LeavesService) {}
 
-  // Apply Leave
+  // Apply leave
   @Post()
-  create(@Body() createLeaveDto: CreateLeaveDto) {
-    return this.leavesService.create(createLeaveDto);
+  create(@Body() dto: CreateLeaveDto) {
+    return this.leavesService.create(dto);
   }
 
-  // Get All Leave Requests
+  // Get all leaves
   @Get()
   findAll() {
     return this.leavesService.findAll();
   }
 
-  // Get Employee Leave Balance
+  // GET ALL BALANCES 
+  @Get('balances')
+  getAllBalances() {
+    return this.leavesService.getAllBalances();
+  }
+
+  // GET SINGLE USER BALANCE
   @Get('balance/:name')
   getBalance(@Param('name') name: string) {
     return this.leavesService.getBalance(name);
   }
 
-  // Update Leave Status (Approve / Reject)
+  // UPDATE LEAVE STATUS
   @Put(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
-    @Body() updateLeaveDto: UpdateLeaveDto,
+    @Body() dto: UpdateLeaveDto,
   ) {
-    return this.leavesService.update(id, updateLeaveDto);
+    return this.leavesService.update(id, dto);
   }
 }
